f70bc53264c7ad3dd6776db1ebcb4268cb226636
h-wal <harsh2102agarwal@gmail.com>
2025-09-05 04:00:07 +0530

    db export and import complete

--- apps/express/src/auth/signup.ts
+++ apps/express/src/auth/signup.ts
@@ -1,6 +1,8 @@
 import express from "express"
 import type {Router, Request, Response} from "express"
 import bcrypt from "bcrypt"
+import db from "@repo/db/client";
+const {prismaClient} = db
 
 const signUpRouter: Router = express.Router()
 
@@ -15,22 +17,22 @@ async function signUpRouterFunction(req: Request, res: Response){
         res.send("Email with this username/ email already found !")
     }
     
-    // if(!exists){
-    //     const createUser = await prismaCleint.create({
-    //         where:{
-    //             username: username,
-    //             password: password
-    //         }
-    //     })
+    if(!exists){
+        const createUser = await prismaClient.user.create({
+            data:{
+                email: email,
+                password: password
+            }
+        })
 
-    //     if(createUser){
-    //         res.status(200).send("User Creation Successfull")
-    //     }
+        if(createUser){
+            res.status(200).send("User Creation Successfull")
+        }
 
-    //     if(!createUser){
-    //         res.status(500).send("Error Creating User")
-    //     }
-    // }
+        if(!createUser){
+            res.status(500).send("Error Creating User")
+        }
+    }
 
 }
 
